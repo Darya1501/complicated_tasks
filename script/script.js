59bd56e0c1a07a0d301daa84ee1ba435e841a5b7
Darya <darya_01@mail.ru>
2021-08-02 15:40:18 +0300

    Выполнено задание 2 урока 3 + исправлены ошибки в предыдущих

--- script/script.js
+++ script/script.js
@@ -10,14 +10,14 @@ while (num > 0) {
 }
 console.log('Произведение цифр числа 266219:', total);
 
-console.log(total, 'в третьей степени:', total ** 3);
+console.log(total + ' в третьей степени = ' + total ** 3);
 
 console.log('Первые две цифры числа', total, '-', total.toString().slice(0, 2));
 
 // Урок 3 - задание 1
 let lang = prompt('Введите язык (ru/en): ');
 
-while (lang !== 'ru' || lang !== 'en') {
+while ((lang !== 'ru') && (lang !== 'en')) {
   lang = prompt('Неверный ввод. Введите "ru" или "en": ');
 }
 
@@ -26,8 +26,7 @@ let enDays = 'Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday';
 
 if (lang === 'ru') {
   console.log(ruDays);
-}
-if (lang === 'ru') {
+} else if(lang === 'en') {
   console.log(enDays);
 }
 
@@ -42,3 +41,10 @@ switch(lang) {
 
 let days = [ ['ru', 'en'], [ruDays, enDays] ];
 console.log( days[1][days[0].indexOf(lang)] );
+
+// Урок 3 - задание 2
+let namePerson = prompt('Введите имя: ');
+
+let result = (namePerson === 'Артем') ? 'Директор' : (namePerson === 'Максим') ? 'Преподаватель' : 'Студент';
+
+console.log('Привет, ', result);
