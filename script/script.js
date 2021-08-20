2ed12a5a6d8879ea0b570f1008705dd0eb3bc04d
Darya <darya_01@mail.ru>
2021-08-21 00:01:03 +0300

    Урок 17 - работа с датой

---
+++ script/script.js
@@ -0,0 +1,44 @@
+const DayOfWeek = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+const now = new Date();
+const newYear = new Date(2022, 0, 1);
+
+
+const getTimesOfDay = function(date) {
+  // const hour = date.getHours();
+  if (date < 4) {
+    return 'Доброй ночи!';
+  } else if (date < 12) {
+    return 'Доброе утро!';
+  } else if (date < 17) {
+    return 'Добрый день!';
+  } else if (date < 24) {
+    return 'Добрый вечер!';
+  } else {
+    return 'Доброй ночи!';
+  }
+};
+
+const wordForm = function(num) {
+  const n = num % 10;
+  if (num >= 5 && num <= 20) {
+    return 'дней';
+  } else if (n === 1) {
+    return 'день';
+  } else if (n >= 2 && n <= 4) {
+    return 'дня';
+  } else {
+    return 'дней';
+  }
+};
+
+const difference = Math.floor(((newYear.getTime() - now.getTime()) / 24 / 60 / 60 / 1000));
+
+const string =
+` <p>${getTimesOfDay(24)} <br>
+Сегодня: ${DayOfWeek[now.getDay()]} <br>
+Текущее время: ${now.toLocaleTimeString('en-US')} <br>
+До нового года осталось ${difference} ${wordForm(difference)}</p> `;
+
+document.body.innerHTML = string;
+
+
